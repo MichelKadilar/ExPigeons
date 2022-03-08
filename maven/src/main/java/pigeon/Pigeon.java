812d599701d747f63706caacd867c3fd84cc1793
Michel Kadilar <mickael92clm@gmail.com>
2022-03-08 19:36:38 +0100

    Initial commit

---
+++ maven/src/main/java/pigeon/Pigeon.java
@@ -0,0 +1,13 @@
+package pigeon;
+
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.reader.InstanceReader;
+import org.sat4j.reader.Reader;
+import org.sat4j.specs.ISolver;
+import org.sat4j.tools.ModelIterator;
+
+public class Pigeon {
+
+
+
+}
